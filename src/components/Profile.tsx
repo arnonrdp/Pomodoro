eae9351834f49ff325100d936a62b9d52e925da8
Arnon Rodrigues <arnonrdp@gmail.com>
2021-02-03 13:51:00 -0300

    Day 1

---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengeContext';
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+    const { level } = useContext(ChallengesContext);
+
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://github.com/arnonrdp.png" alt="Dariane Rodrigues"/>
+            <div>
+                <strong>Dariane Rodrigues</strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level"/>
+                    Level {level}
+                </p>
+            </div>
+        </div>
+    );
+}
